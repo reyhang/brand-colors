2463f1e61c9b1c9f19cf561ed662f40d246075f4
reyhang <reyhanguneyy@outlook.com>
2022-10-29 20:20:40 +0300

    Modal eklendi ve logo düzenlendi.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './sidebar.scss'
+import Modal from 'react-modal'
+import { GrClose } from "react-icons/gr";
 
-export class Sidebar extends Component {
-    render() {
-        return (
-            <aside className='sidebar'>
-                <div className='logo'>
-                    <a href='!#'>Brand<b>Colors</b></a>
-                </div>
 
-                <div className='description'>
-                    The biggest collection of official brand color codes around.
-                    Curated by @brandcolors and friends. </div>
-                <nav className="menu">
-                    <ul>
-                        <li>
-                            <a href='!#'>About Brand Colors</a>
-                        </li>
-                    </ul>
-                </nav>
-            </aside>
+export default function Sidebar() {
 
-        )
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal)
     }
-}
 
-export default Sidebar
+
+    return (<>
+        <aside className='sidebar'>
+            <div className='logo'>               
+             <a href='!#'>Brand<b>Colors</b></a>
+            </div>
+
+            <div className='description'>
+                The biggest collection of official brand color codes around.
+                Curated by @brandcolors and friends. </div>
+            <nav className="menu">
+                <ul>
+                    <li>
+                        <a onClick={toggleModal} href='!#'>About Brand Colors</a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+
+        <Modal
+            isOpen={modal}
+            onRequestClose={toggleModal}
+            className="modal"
+            overlayClassName="modal-overlay"
+            contentLabel="Example Modal"> <button className='modal-close-btn' onClick={toggleModal}><GrClose /></button>
+            <h2> About BrandColors </h2>
+
+            <p>
+                BrandColors was created by
+                <a href='!#'>
+                    <strong> DesignBombs</strong>
+                </a>.
+                The goal was to create a helpful reference for the brand color codes that are needed most often.
+                <br /><br />
+                It's been featured by Smashing Magazine, CSS-Tricks, Web Design Depot, Tuts+,
+                and over <strong> 2 million pageviews </strong> .
+                There are now over <strong>600 brands</strong>  with
+                <strong> 1600 colors </strong> and the collection is always growing.
+            </p>
+
+
+        </Modal>
+    </>
+    )
+}   
+
+
