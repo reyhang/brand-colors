1420901234e1e7b62e6afdeee3ad4f1cd538c5d5
reyhang <reyhanguneyy@outlook.com>
2022-11-04 17:16:29 +0300

    Context oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import Content from "./components/Content/Content";
 import Sidebar from "./components/Sidebar/Sidebar";
+import BrandsData from './brands.json'
+import MainContext from "./MainContext";
+
 
 
 function App() {
+
+  const brandsArray = []
+  Object.keys(BrandsData).map(key =>
+    (brandsArray.push(BrandsData[key]))
+  )
+
+  const [brands, setBrands] = useState(brandsArray)
+  const [selectedBrands, setSelectBrands] = useState([])
+  
+  const data = {
+    brands,
+    setSelectBrands,
+  }
+
   return (
     <>
-     <Sidebar/>
-     <Content/>
+      <MainContext.Provider value={data}>
+        <Sidebar />
+        <Content />
+      </MainContext.Provider>
+
     </>
-    
+
   );
 }
 
