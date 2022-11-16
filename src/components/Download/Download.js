36fc1a406b992853e9c7acf8407ebab0eaa11d21
reyhang <reyhanguneyy@outlook.com>
2022-11-16 18:07:35 +0300

    Download componenti geliştirildi.

--- src/components/Download/Download.js
+++ src/components/Download/Download.js
@@ -1,13 +1,37 @@
 import React, { useContext } from 'react'
 import MainContext from '../../MainContext'
+import './download.scss'
+import { FiDownload,FiLink2,FiX } from 'react-icons/fi'
 
 export default function Download() {
 
-    const { selectedBrands } = useContext(MainContext)
+    const { selectedBrands, setSelectedBrands } = useContext(MainContext)
+
+
 
     return (
         <div className='download'>
-            {selectedBrands.length} {selectedBrands.length === 1 ? 'brand' : 'brands'} collected.
+
+            <div className='actions'>
+                <button>
+                <FiDownload  color='rgb(195, 90, 255)' size={'23px'} />
+            </button>
+                <a href='!#'>
+                    <FiLink2 color='rgb(195, 90, 255)' size={'25px'}/>
+                </a>
+               
+            </div>
+
+            <div className='selected'>
+                <button onClick={()=> setSelectedBrands([])}>
+                    <FiX color='rgb(195, 90, 255)' size={'23px'} />
+                </button>
+                <p style={selectedBrands.length === 0 ? { color: 'gray' } : { color: 'rgb(195, 90, 255)' }}>
+                    {' '} {selectedBrands.length}
+                    {selectedBrands.length === 1 || selectedBrands.length === 0 ? ' brand' : ' brands'} collected {' '}
+                </p>
+            </div>
+
         </div>
     )
 }
