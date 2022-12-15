b2e2aa8f7910a031e3d46668a03de707fa2b14fa
reyhang <reyhanguneyy@outlook.com>
2022-12-15 17:48:24 +0300

    Collection componenti tamamlandı.

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -1,49 +1,68 @@
-import React, { useContext } from 'react'
+import { createBrowserHistory } from '@remix-run/router'
+import React, { useContext, useEffect } from 'react'
 import { FaSpinner } from 'react-icons/fa'
+import { GrLinkPrevious } from 'react-icons/gr'
 import LazyLoad from 'react-lazyload'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import MainContext from '../../MainContext'
 import Brand from '../Brand/Brand'
 import Download from '../Download/Download'
-import Search from '../Search/Search'
+import './collection.scss'
 
 export default function Collection() {
 
-  const {slugs} = useParams()  
-  const {brands, selectedBrands} = useContext(MainContext)
+  const { slugs } = useParams()
+  const { brands, selectedBrands, setSelectedBrands } = useContext(MainContext)
+  const history = createBrowserHistory()
 
   const loading = () => {
-    return(
+    return (
       <div className='loading'>
         <span >
           <FaSpinner size={"25px"} />
-        </span>                            
+        </span>
       </div>
-      
+
     )
   }
-    return (
-        <main className='content'>
-            <header className='header'>
-                <Search />
-                <Download />
 
-            </header>
+  const clearSelectedBrands = () => {
+    setSelectedBrands([])
+    history.push('/')
+  }
 
-            {
-                selectedBrands.map((slug) => {
-                    let brand = brands.find((brand) => brand.slug === slug)
-                    return (
-                        <LazyLoad once={brand.once} placeholder={loading()} overflow={true} >
-                            <Brand brand={brand} />
-                        </LazyLoad>
-                    )
+  useEffect(() => {
+    setSelectedBrands(slugs.split(','))
+    //eslint-disable-next-line
+  }, [])
 
-                }
-                )
-            }
-       
+  return (
+    <main className='content'>
 
-  </main>
+      <header className='header'>
+        <Link to="/" onClick={clearSelectedBrands}>
+          <a href='!#' className="back-button">
+            <GrLinkPrevious />
+            All Brands
+          </a>
+        </Link>
+        {selectedBrands.length !== 0 && <Download />}
+      </header>
+
+      {
+        selectedBrands.map((slug) => {
+          let brand = brands.find((brand) => brand.slug === slug)
+          return (
+            <LazyLoad once={brand.once} placeholder={loading()} overflow={true} >
+              <Brand brand={brand} />
+            </LazyLoad>
+          )
+
+        }
+        )
+      }
+
+
+    </main>
   )
 }
